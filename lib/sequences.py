4b987afea6f9028fcfc3f51380272a03869fab27
Galen Sho Sato <galen.sato@gmail.com>
2023-02-21 09:46:32 -0500

    Finished Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+    if length == 0:
+        print("[]")
+    elif length > 0:
+        print(fib[0:length])
+
+
